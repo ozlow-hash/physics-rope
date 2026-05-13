9940e1cdd585e08f8a64bc09c6016192505c5e42
ozlow-hash <austinlegg@gmail.com>
2026-05-12 21:09:32 -0700

    added ball physics

--- player.gd
+++ player.gd
@@ -3,7 +3,7 @@ extends Node2D
 var draw_start: Vector2
 var draw_held: Vector2
 var draw_end: Vector2
-var mouse_drag = false
+var dragging = false
 
 var lines = [] #stores compelted liens
 
@@ -13,32 +13,37 @@ func _ready() -> void:
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
-		draw_start = event.position
 		if event.is_action_pressed("Draw"):
-			draw_held = event.position
-			mouse_drag = true
-		if event.is_action_released("Draw"):
-			mouse_drag = false
-			draw_end = event.position
+			dragging = true
+			draw_start = get_global_mouse_position()
+			draw_held = get_global_mouse_position()
+			queue_redraw()
+		else:
+			dragging = false
+			draw_end = get_global_mouse_position()
 			lines.append({"start": draw_start, "end": draw_end})
+			
+			#create collision
+			create_line_collision(draw_start, draw_end)
+			
+			queue_redraw()
+	elif InputEventMouseMotion and dragging:
+		draw_held = get_global_mouse_position()
+		queue_redraw()
+
+func create_line_collision(start: Vector2, end: Vector2):
+	var static_body = StaticBody2D.new()
+	var collision_shape = CollisionShape2D.new()
+	var segment = SegmentShape2D.new()
 	
-		
-		#if event.is_action_just_pressed("Draw"):
-			#draw_start = event.position
-			#lines.append(draw_start)
-		#if Input.is_action_pressed("Draw"):
-			#draw_held = event.position
-			#mouse_drag = true
-			#queue_redraw()
-		#if Input.is_action_just_released("Draw"):
-			#draw_end = event.position
-			#mouse_drag = false
-			#lines.append(draw_end)
-		
-		print(lines)
+	segment.a = start
+	segment.b = end
+	
+	static_body.add_child(collision_shape) #makes collision shape child of static body
+	add_child(static_body)
 
 func _draw() -> void:
-	if mouse_drag == false:
-		draw_line(draw_start, draw_end, Color.WHITE, 20.0)
-	else:
-		draw_line(draw_start, draw_held, Color.RED, 10)
+	for line in lines:
+		draw_line(line["start"], line["end"], Color.ALICE_BLUE, 4, true)
+	if dragging:
+		draw_line(draw_start, draw_held, Color.RED, 2, true)
