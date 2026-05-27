4ca4eb4b909bba3f95fe95193c3270261914a451
ozlow-hash <austinlegg@gmail.com>
2026-05-26 17:37:21 -0700

    added col to drawn line

--- player.gd
+++ player.gd
@@ -5,7 +5,10 @@ var draw_held: Vector2
 var draw_end: Vector2
 var dragging = false
 
-var lines = [] #stores compelted liens
+var line_length = 0
+#var length = draw_start.distance_to(draw_end)
+
+var drawn_lines = [] #stores completed liens
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -21,65 +24,40 @@ func _input(event: InputEvent) -> void:
 		elif event.is_action_released("Draw"): #get the second point of the line
 			dragging = false
 			draw_end = get_global_mouse_position()
-			lines.append({"start": draw_start, "end": draw_end})
-			
+			drawn_lines.append({"start": draw_start, "end": draw_end})
+			line_length = draw_start.distance_to(draw_end)
+			create_line_collision()
 			
 			queue_redraw()
 	elif event is InputEventMouseMotion and dragging: #draw the preview of the line
 		draw_held = get_global_mouse_position()
 		queue_redraw()
-	if event is InputEventKey:
-		if Input.is_key_pressed(KEY_SPACE):
-			print("test")
-			create_line_collision()
+
 
 func create_line_collision():
 	var static_body = StaticBody2D.new()
-	var shape = RectangleShape2D.new()
-	var length = draw_start.distance_to(draw_end)
-	var height = 40
-	var angle = (draw_end - draw_start).angle()
 	
+	#create shape and size of collision
+	var shape = RectangleShape2D.new()
+	var height = 10
+	var col_shape = Vector2(line_length,height)
+	shape.size = col_shape
+	
+	#set angle of collision
+	var angle = (draw_end - draw_start).angle()
 	static_body.rotation = angle
+	
+	#set position of collision
 	static_body.position = (draw_start + draw_end) / 2
 	add_child(static_body)
 	
+	#create collision
 	var collision_shape = CollisionShape2D.new()
 	static_body.add_child(collision_shape)
-	
 	collision_shape.shape = shape
 
 func _draw() -> void:
-	for line in lines:
+	for line in drawn_lines:
 		draw_line(line["start"], line["end"], Color.ALICE_BLUE, 4, true)
 	if dragging:
 		draw_line(draw_start, draw_held, Color.RED, 2, true)
-
-func _create_static_body():
-	##create staticbody2d Node
-	#var static_body = StaticBody2D.new()
-	#static_body.global_position = Vector2(0,0)
-	#
-	#var col_shape = CollisionShape2D.new()
-	#var shape = RectangleShape2D.new()
-	#shape.size = Vector2(100,20)
-	#
-	##connect it all together
-	#col_shape.shape = shape
-	#static_body.add_child(col_shape)
-	#
-	#add_child(static_body)
-	#
-	var static_body = StaticBody2D.new()
-	var collision_shape = CollisionShape2D.new()
-	var segment = SegmentShape2D.new()
-	
-	segment.a = draw_start
-	segment.b = draw_end
-	
-	collision_shape.shape = segment
-	
-	static_body.add_child(collision_shape)
-	
-	get_parent().add_child(static_body)
-	
